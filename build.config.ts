23ba56aaeba5ee8de65283031807168a3c7b548b
Sasha Sorokin <10401817+Brawaru@users.noreply.github.com>
2022-12-29 13:50:40 +0100

    fix: transform to support environments
    Enabled transformation using Babel to support older environments.

--- build.config.ts
+++ build.config.ts
@@ -1,8 +1,47 @@
 // build.config.ts
 import { defineBuildConfig } from 'unbuild'
+import { babel } from '@rollup/plugin-babel'
 
 export default defineBuildConfig({
   replace: {
     'import.meta.vitest': 'undefined',
+  },
+  hooks: {
+    'rollup:options'(_ctx, options) {
+      if (!Array.isArray(options.plugins)) {
+        if (options.plugins != null && typeof options.plugins !== 'boolean') {
+          options.plugins = [options.plugins]
+        } else {
+          options.plugins = []
+        }
+      }
+
+      for (let i = 0, l = options.plugins.length; i < l; i++) {
+        const plugin = options.plugins[i]
+
+        if (
+          typeof plugin === 'object' &&
+          plugin != null &&
+          !Array.isArray(plugin) &&
+          !('then' in plugin)
+        ) {
+          if (plugin.name === 'esbuild') {
+            options.plugins[i] = babel({
+              babelHelpers: 'bundled',
+              extensions: ['.ts', '.js', '.cjs', '.mjs', '.es', '.es6'],
+              presets: [
+                '@babel/preset-typescript',
+                [
+                  '@babel/preset-env',
+                  {
+                    targets: '>= .3% and not dead, node > 16',
+                  },
+                ],
+              ],
+            })
+          }
+        }
+      }
+    },
   },
 })
